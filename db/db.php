20c113951d7eb8a2e5c2fe98f0dc8f2ef754993d
sosokkrota <ganzikmail09@gmail.com>
2015-06-22 23:46:03 +0300

    Update db.php

--- db/db.php
+++ db/db.php
@@ -1,5 +1,35 @@
 <?php
+  require_once "config.php" ;
+
   class db extends config {
+    private $connection ;
     
+    function __construct() {
+      $this->open_connection() ;
+    }
+    
+    private function open_connection() {
+      $this->connection = mysql_connect($this->$DB_HOST , $this->DB_USER , $this->DB_PASS )
+      if(!$this->connection)
+      {
+        die("Database connection failed:".mysql_error()) ;
+      }else{
+        $db_select = mysql_select_db($this->DB_NAME) ;
+        if(!$db_select){
+          die("Database selection failed:".mysql_error())
+        }
+      }
+      mysql_query("set names utf8") or die("set names utf8 failed") ;
+    }
+    
+    public function sql($query) {
+      $result = mysql_query($query, $this->connection) ;
+      if(!$result) {
+        die("Database query failed:".mysql_error()) ;
+      }
+      return $result ;
+    }
   }
+  
+  $db = new db() ;
 ?>
